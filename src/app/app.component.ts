5ae68ba1e12189d931afd49ab1aae997ff6167a5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-28 14:35:56 +0300

    refactor : update test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from "./test/test.component";
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TestComponent],
+  imports: [TestComponent, Test2Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'petrosgram';
