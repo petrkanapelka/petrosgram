9303672ce6494fb4110dc220499bf1dabce3f650
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-28 14:34:01 +0300

    feat: create test components

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
